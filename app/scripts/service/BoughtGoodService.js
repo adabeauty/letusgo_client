64929813faa7b3472095c2b8b35b2df545d78f2b
adabeauty <654296217@qq.com>
2014-09-21 17:24:31 +0800

    storage boughtGoods in server

--- app/scripts/service/BoughtGoodService.js
+++ app/scripts/service/BoughtGoodService.js
@@ -1,26 +1,6 @@
 'use strict';
 angular.module('letusgoApp').service('BoughtGoodsService', function (localStorageService, $http) {
 
-    function getClickCount (callback){
-
-        $http.get('/api/clickCount').success(function(data){
-            callback(JSON.parse(data));
-        });
-    }
-
-    function setClickCount(clickCount, callback){
-
-        $http.post('/api/clickCount', {'clickCount': clickCount}).success(function(data){
-            callback(data);
-        });
-
-    }
-    this.getClickCount = function(callback){
-        getClickCount(function(data) {
-            callback(data);
-        });
-    };
-
     this.BoughtItem = function (item, num) {
         return {    num: num,
             item: item
@@ -28,9 +8,9 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
     };
 
     this.hasExistGoods = function (name, boughtGoods) {
-        var boughtGood;
 
-        if(boughtGoods !== null){
+        var boughtGood;
+        if(boughtGoods != []){
             for (var i = 0; i < boughtGoods.length; i++) {
 
                 if (name === boughtGoods[i].item.name) {
@@ -41,53 +21,56 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
 
         return boughtGood;
     };
-    this.addClickcount = function (direction, number,callback) {
 
-        var addClickCount = function(clickcount){
-            direction === 1 ? clickcount = clickcount + number : clickcount = clickcount - number;
-            return clickcount;
-        };
-//        var currentThis = this;
-        this.getClickCount(function(getData){
-
-            var currentClickCount = addClickCount(getData);
-
-            setClickCount(currentClickCount, function(setDate){
-                console.log(currentClickCount);
-                if(setDate === 'ok'){
-                    callback(currentClickCount);
-                }
-            });
-            callback(currentClickCount);
-        });
-
-//        var clickcount = +localStorageService.get('clickcount');
-//
-//        direction === 1 ? clickcount = clickcount + number : clickcount = clickcount - number;
-//
-//        localStorageService.set('clickcount', clickcount);
-//        return clickcount;
-    };
     this.addCartNum= function (item) {
+        var currentThis = this;
+        $http.get('api/boughtGoods').success(function(data){
 
-        var boughtGoods = localStorageService.get('boughtGoods');
+            var boughtGoods = data;
+// 放在server里           if (boughtGoods === 'nil') {
+//                boughtGoods = [];
+//            }
 
-        if (localStorageService.get('boughtGoods') === null) {
-            boughtGoods = [];
-        }
-        var boughtGood = this.hasExistGoods (item.name, boughtGoods);
+            var boughtGood = currentThis.hasExistGoods (item.name, boughtGoods);
+            if(boughtGood){
+                boughtGood.num++
+            }else{
+                boughtGoods.push(currentThis.BoughtItem(item, 1));
+            }
 
-        boughtGood ? boughtGood.num++ : boughtGoods.push(this.BoughtItem(item, 1));
+            $http.post('/api/boughtGoods', {'boughtGoods': boughtGoods}).success(function(currentBoughtGoods){
+                localStorageService.set('boughtGoods', boughtGoods);
 
-        localStorageService.set('boughtGoods', boughtGoods);
+            });
+        });
+//        var boughtGoods = localStorageService.get('boughtGoods');
+//
+//        if (localStorageService.get('boughtGoods') === null) {
+//            boughtGoods = [];
+//        }
+//        var boughtGood = this.hasExistGoods (item.name, boughtGoods);
+//
+//        boughtGood ? boughtGood.num++ : boughtGoods.push(this.BoughtItem(item, 1));
+//
+//        localStorageService.set('boughtGoods', boughtGoods);
     };
+
     this.cartList = function (className, boughtgoods) {
 
         return {    categoryName: className,
             boughtgoods: boughtgoods
         };
     };
-    this.getGoodsArray = function(){
+    this.getGoodsArray = function(callback){
+//        $http.get('/api/boughtGoods').success(function(data){
+//            var boughtGoods = JSON.parse(data);
+//            var goodsObject = _.groupBy(boughtGoods, function (num) {
+//                return num.item.category;
+//            });
+//            var goodsArray = _.map(goodsObject);
+//
+//            callback(goodsArray);
+//        });
         var boughtGoods = localStorageService.get('boughtGoods');
         var goodsObject = _.groupBy(boughtGoods, function (num) {
             return num.item.category;
@@ -134,6 +117,74 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
 
         return totalMoney;
     };
+
+    this.getClickCount = function(data){
+//        var totalCount = _.reduce(data, function(totalCount, every) {
+//            return totalCount + every.num;
+//        });
+//        return totalCount;
+        var sum = 0;
+        if(data !== []){
+            _.forEach(data, function (item) {
+                sum += item.num;
+            });
+        }
+        console.log('sum ', sum);
+        return sum;
+    };
+
+    this.refreshData = function(callback){
+        var currentThis = this;
+        $http.get('/api/boughtGoods').success(function(data){
+
+            var result = {
+                totalAmount: currentThis.getTotalMoney(data),
+                cartGoods: currentThis.generateCartGoods(data),
+                totalCount: currentThis.getClickCount(data)
+            };
+            console.log('result:',result);
+            callback(result);
+        });
+    };
+    this.addClickcount = function (direction, number,callback) {
+
+        var addClickCount = function(clickcount){
+            direction === 1 ? clickcount = clickcount + number : clickcount = clickcount - number;
+            return clickcount;
+        };
+
+        var currentThis = this;
+        $http.get('/api/boughtGoods').success(function(data){
+
+            var clickCount = currentThis.getClickCount(data);
+            var currentClickCount = addClickCount(clickCount);
+            callback(currentClickCount);
+        });
+//        var addClickCount = function(clickcount){
+//            direction === 1 ? clickcount = clickcount + number : clickcount = clickcount - number;
+//            return clickcount;
+//        };
+//
+//        this.getClickCount(function(getData){
+//
+//            var currentClickCount = addClickCount(getData);
+//
+//            setClickCount(currentClickCount, function(setDate){
+//                console.log(currentClickCount);
+//                if(setDate === 'ok'){
+//                    callback(currentClickCount);
+//                }
+//            });
+//            callback(currentClickCount);
+//        });
+
+//        var clickcount = +localStorageService.get('clickcount');
+//
+//        direction === 1 ? clickcount = clickcount + number : clickcount = clickcount - number;
+//
+//        localStorageService.set('clickcount', clickcount);
+//        return clickcount;
+    };
     this.getboughtGoodsLength = function () {
         var boughtGoods = localStorageService.get('boughtGoods');
         return  boughtGoods.length;
@@ -174,6 +225,8 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
                 this.processNum(direction, i);
             }
         }
+        $http.post('/api/boughtGoods', {'boughtGoods': boughtGoods}).success(function(){});
+
     };
     this.deleteItem = function (cartItem) {
         var boughtGoods = localStorageService.get('boughtGoods');
@@ -189,15 +242,22 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
     this.clearDate = function () {
 
 //        localStorageService.set('clickcount', 0);
-        setClickCount(0, function(data){
-            if(data === 'ok'){
-                callback(data);
-            }
+//        setClickCount(0, function(data){
+//            if(data === 'ok'){
+//                callback(data);
+//            }
+//        });
+        $http.post('/api/boughtGoods', {'boughtGoods': []}).success(function(data){
+
+            localStorageService.set('boughtGoods', []);
+            localStorageService.set('drinks', 0);
+            localStorageService.set('snacks', 0);
+            localStorageService.set('nuts', 0);
         });
-        localStorageService.set('boughtGoods', '');
-        localStorageService.set('drinks', 0);
-        localStorageService.set('snacks', 0);
-        localStorageService.set('nuts', 0);
+//        localStorageService.set('boughtGoods', '');
+//        localStorageService.set('drinks', 0);
+//        localStorageService.set('snacks', 0);
+//        localStorageService.set('nuts', 0);
     };
 
 });
