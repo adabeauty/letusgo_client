5fffe2883209d80a9c293de1e6411bde91ed9bb8
adabeauty <654296217@qq.com>
2014-09-23 23:49:12 +0800

    put categories in server

--- app/scripts/service/categoryManageService.js
+++ app/scripts/service/categoryManageService.js
@@ -7,82 +7,75 @@ angular.module('letusgoApp').service('CategoryService', function (localStorageSe
             num: num
         };
     };
-    this.getCurrentID = function(){
+    this.getCurrentID = function(callback){
 
-        var category = localStorageService.get('category');
-        if(category.length === 0){
-            return 1;
-        }
-        var lastID = category[category.length - 1].ID;
-        var currentID = JSON.parse(lastID) + 1;
-        return currentID;
+        $http.get('/api/categories').success(function(categories){
+            console.log(categories);
+            if(categories.length === 0){
+                callback(1);
+            }else{
+                var lastID = categories[categories.length - 1].ID;
+                var currentID = JSON.parse(lastID) + 1;
+                callback(currentID);
+            }
+        });
     };
 
-    this.nameHadExist = function (currentName) {
+    this.nameHadExist = function (categories, currentName) {
 
-        var currentCategory = localStorageService.get('category');
-        var nameExist = _.findIndex(currentCategory, {name: currentName});
+        var nameExist = _.findIndex(categories, {name: currentName});
 
         return nameExist;
     };
-    this.addNewCateogory = function (currentID, currentName) {
-
-        var currentCategory = localStorageService.get('category');
-        var noCategory = currentCategory === '' || currentCategory === null;
-        if (noCategory) {
-            currentCategory = [];
-        }
+    this.addNewCateogory = function (categories, currentID, currentName) {
 
         var current = this.category(currentID, currentName, '0');
 
-        currentCategory.push(current);
-        $http.post('/api/categories', {'categories': currentCategory}).success(function(){});
-        localStorageService.set('category', currentCategory);
+        categories.push(current);
+        $http.post('/api/categories', {'categories': categories}).success(function(){});
 
     };
 
     this.saveButton = function (currentID, currentName) {
 
-        var nameHadExist = this.nameHadExist(currentName);
-        if (!currentName) {
-            alert('请填写分类名称!');
-            return false;
-        }
-        if (nameHadExist !== -1) {
-            alert('此商品分类已经存在,请重新输入!');
-            return false;
-        } else{
-            this.addNewCateogory(currentID, currentName);
-            $location.path('/categoryManage');
-            return true;
-        }
+        var currentThis = this;
+        $http.get('/api/categories').success(function(categories){
+            var nameHadExist = currentThis.nameHadExist(categories, currentName);
+            if (!currentName) {
+                alert('请填写分类名称!');
+            }
+            if (nameHadExist !== -1) {
+                alert('此商品分类已经存在,请重新输入!');
+            } else{
+                currentThis.addNewCateogory(categories, currentID, currentName);
+                $location.path('/categoryManage');
+            }
+        });
     };
 
-    this.updateCategory = function () {
+    this.updateCategory = function (callback) {
 
-        var updateObeject = localStorageService.get('updateCategory');
-        var allCategories = localStorageService.get('category');
-        var index = _.findIndex(allCategories, {'ID': updateObeject.ID});
-        allCategories[index] = updateObeject;
+        $http.get('/api/categories').success(function(categories){
 
-        $http.post('/api/categories', {'categories': allCategories}).success(function(){});
-        localStorageService.set('category', allCategories);
-        return index;
+            var updateObeject = localStorageService.get('updateCategory');
+            var index = _.findIndex(categories, {'ID': updateObeject.ID});
+            categories[index] = updateObeject;
+            $http.post('/api/categories', {'categories': categories}).success(function(){});
+            callback();
+        });
+
     };
 
-    this.deleteButton = function (every) {
+    this.deleteButton = function (categories, object) {
 
-        var currentCategory = localStorageService.get('category');
-
-        if (every.num !== 0) {
+        if (object.num !== '0') {
             alert('此分类下有商品存在,不能删除');
         } else {
-            var events = _.remove(currentCategory, function (num) {
-                return every.ID === num.ID;
+            var events = _.remove(categories, function (every) {
+                return object.ID === every.ID;
             });
         }
-        $http.post('/api/categories', {'categories': currentCategory}).success(function(){});
-        localStorageService.set('category', currentCategory);
+        $http.post('/api/categories', {'categories': categories}).success(function(){});
 
     };
 
