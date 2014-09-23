6a034fb15d667d6da79a07e22472c41ca8f04e37
adabeauty <654296217@qq.com>
2014-09-23 16:02:23 +0800

    get categories from server

--- app/scripts/controllers/categoryManage.js
+++ app/scripts/controllers/categoryManage.js
@@ -1,13 +1,16 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .controller('CategoryCtrl', function ($scope, CategoryService, localStorageService) {
+    .controller('CategoryCtrl', function ($scope, CategoryService, localStorageService, $http) {
 
         $scope.$emit('to-parent-navigator-incategoryManage');
         $scope.$emit('to-parent-changeClickCount', 1, 0);
 
-        $scope.category = localStorageService.get('category');
-
+//        $scope.category = localStorageService.get('category');
+        $http.get('/api/categories').success(function(data){
+            $scope.category = data;
+            localStorageService.set('category', data);
+        });
         $scope.editButton = function (categoryDetail) {
 
             localStorageService.set('updateCategory', categoryDetail);
