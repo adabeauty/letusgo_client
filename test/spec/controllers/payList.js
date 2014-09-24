4854cc46ed95a3f4140947e837fb48240ca5c115
adabeauty <654296217@qq.com>
2014-09-24 23:14:21 +0800

    finish payList test

--- test/spec/controllers/payList.js
+++ test/spec/controllers/payList.js
@@ -1,54 +1,56 @@
-// 'use strict';
-xdescribe('payList test: ', function () {
+'use strict';
+describe('payList test: ', function () {
 
     beforeEach(module('letusgoApp'));
 
-    var $scope, BoughtGoodsService, localStorageService, $controller;
+    var $scope, BoughtGoodsService, $http, $httpBackend, localStorageService, $controller, creatPayListCtrl;
 
     beforeEach(inject(function ($injector) {
 
         $scope = $injector.get('$rootScope').$new();
+        $http = $injector.get('$http');
+        $httpBackend = $injector.get('$httpBackend');
         BoughtGoodsService = $injector.get('BoughtGoodsService');
         localStorageService = $injector.get('localStorageService');
 
         $controller = $injector.get('$controller');
-
         creatPayListCtrl = function () {
 
             return $controller('PayListCtrl', {
                 $scope: $scope,
                 BoughtGoodsService: BoughtGoodsService,
-                localStorageService: localStorageService
+                localStorageService: localStorageService,
+                $http: $http
             });
         }
 
     }));
-
-    describe('localStorageService', function () {
+    beforeEach(function(){
+        spyOn($scope, '$emit');
+        $httpBackend.when('GET', '/api/boughtGoods').respond([{}, {}, {}]);
+        creatPayListCtrl();
+    });
+    describe('outside function', function(){
+        beforeEach(function(){
+            spyOn(BoughtGoodsService, 'getTotalMoney');
+        });
+        it('should work', function(){
+            expect($scope.$emit).toHaveBeenCalledWith('to-parent-changeClickCount', 1, 0);
+//            expect(BoughtGoodsService.getTotalMoney).toHaveBeenCalled();
+            $httpBackend.expectGET('/api/boughtGoods');
+            $httpBackend.flush();
+        });
+    });
+    describe('clearData', function () {
 
         beforeEach(function () {
-            spyOn(localStorageService, 'get');
-            spyOn(BoughtGoodsService, 'getboughtGoodsLength');
-            spyOn(BoughtGoodsService, 'getTotalMoney');
-
             spyOn(BoughtGoodsService, 'clearDate');
-            spyOn($scope, "$emit");
-            creatPayListCtrl();
-
-        });
-
-        it('localStorageService.get is ok', function () {
-
-            expect(localStorageService.get).toHaveBeenCalled();
-            expect(BoughtGoodsService.getboughtGoodsLength).toHaveBeenCalled();
-            expect(BoughtGoodsService.getTotalMoney).toHaveBeenCalled();
-
             $scope.clearDate();
+        });
+        it('should work', function () {
             expect(BoughtGoodsService.clearDate).toHaveBeenCalled();
             expect($scope.$emit).toHaveBeenCalledWith('to-parent-clearClickCount');
-
         });
-
     });
 
 
