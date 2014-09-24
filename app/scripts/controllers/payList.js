df610765c657a216767e3b3589c42951bcff9754
adabeauty <654296217@qq.com>
2014-09-24 23:13:53 +0800

    add emit clickCout

--- app/scripts/controllers/payList.js
+++ app/scripts/controllers/payList.js
@@ -4,14 +4,14 @@ angular.module('letusgoApp')
     .controller('PayListCtrl', function ($scope, BoughtGoodsService, localStorageService, $http) {
 
         $http.get('/api/boughtGoods').success(function(data){
-
             $scope.boughtGoods = data;
             $scope.boughtGoodsLength = data.length;
             $scope.totalMoney = BoughtGoodsService.getTotalMoney(data);;
         });
 
-        $scope.clearDate = function () {
+        $scope.$emit('to-parent-changeClickCount', 1, 0);
 
+        $scope.clearDate = function () {
             BoughtGoodsService.clearDate();
             $scope.$emit('to-parent-clearClickCount');
         };
