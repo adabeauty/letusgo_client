1f05f4f9f7e806050d3853f8859f9c3eb1572aa5
adabeauty <654296217@qq.com>
2014-09-26 23:15:44 +0800

    update shop test with callback

--- test/spec/controllers/shop.js
+++ test/spec/controllers/shop.js
@@ -25,19 +25,24 @@ describe('Controller: ShopCtrl', function () {
         };
     }));
     beforeEach(function(){
+        var item = {name:'可口可乐', category: 'drinks', price: 3.50, num:1};
         spyOn($scope, '$emit');
-        $httpBackend.when('GET', '/api/goods').respond([{}, {}, {}]);
+        $httpBackend.when('GET', '/api/goods').respond(item);
         creatShopCtrl();
     });
     describe('outside function', function(){
         it('should work', function(){
             expect($scope.$emit).toHaveBeenCalledWith('to-parent-navigator-inshop');
             expect($scope.$emit).toHaveBeenCalledWith('to-parent-changeClickCount', 1, 0);
+
+            $http.get('/api/goods').success(function(goods){
+              expect($scope.allItems.category).toEqual('drinks');
+            });
             $httpBackend.expectGET('/api/goods');
             $httpBackend.flush();
         });
     });
-    ddescribe('addCartNum', function () {
+    describe('addCartNum', function () {
 
         var item = {category: '饮料类', name: '可口可乐', price: '3.00', unit: '瓶'};
         beforeEach(function () {
