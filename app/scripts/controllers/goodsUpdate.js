d458d8d2d7d079ae74152c7ae0cf0e5df1585f8d
adabeauty <654296217@qq.com>
2014-09-26 17:57:48 +0800

    rename cancel to closeUpdateView

--- app/scripts/controllers/goodsUpdate.js
+++ app/scripts/controllers/goodsUpdate.js
@@ -10,7 +10,7 @@ angular.module('letusgoApp')
 
         };
 
-        $scope.cancel = function () {
+        $scope.closeUpdateView = function () {
             $location.path('/goodsManage');
         };
     });
