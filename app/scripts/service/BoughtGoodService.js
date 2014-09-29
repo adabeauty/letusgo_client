dd0b83ad6e8632504a815e4825fe000ecdc1a8b4
adabeauty <654296217@qq.com>
2014-09-29 09:35:35 +0800

    update postpayment

--- app/scripts/service/BoughtGoodService.js
+++ app/scripts/service/BoughtGoodService.js
@@ -140,7 +140,7 @@ angular.module('letusgoApp').service('BoughtGoodsService', function (localStorag
     };
 
     this.clearDate = function () {
-        $http.post('/api/payment', {'emptyData': []});
+        $http.post('/api/payment');
     };
 
 });
